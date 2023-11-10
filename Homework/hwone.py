434883cc2e4127397f5d7924ff77f2e989c9c756
Bermet0 <bekatktbkv@gmail.com>
2023-11-10 22:14:27 +0600

    Greate-bot

--- Homework/hwone.py
+++ Homework/hwone.py
@@ -7,14 +7,6 @@ import os
 from pathlib import Path
 from random import choice
 
-ml = []
-
-p = Path("images/")
-for c in p.iterdir():
-    ml.append(c)
-    print(c)
-
-choice(ml)
 
 BOT_TOKEN = '6476302799:AAFcNT6L0Ov_-FOGkzwBRe4A7b3R9d-dRv4'
 bot = Bot(token=BOT_TOKEN)
@@ -32,18 +24,18 @@ async def echo(message: types.Message):
         # await message.answer("hi")
     await message.answer(f" Your name is: {message.from_user.first_name}, Your id is: {message.from_user.id}")
 
-@dp.message(Command("picture"))
-async def picture(message: types.Message):
-    file = types.FSInputFile(choice(ml))
-    await message.answer_photo(
-        photo=file,
-    )
-
 # @dp.message(Command("picture"))
 # async def picture(message: types.Message):
-#     image = random.choice(os.listdir(path=".\images"))
-#     images = types.FSInputFile(f'./images/{image}')
-#     await message.reply_photo(photo=images)
+#     file = types.FSInputFile(choice(ml))
+#     await message.answer_photo(
+#         photo=file,
+#     )
+
+@dp.message(Command("picture"))
+async def picture(message: types.Message):
+    image = random.choice(os.listdir(path=".\images"))
+    images = types.FSInputFile(f'./images/{image}')
+    await message.reply_photo(photo=images)
 
 
 
