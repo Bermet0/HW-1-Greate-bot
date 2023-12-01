4918dd0afbddfa5bf0521ca194606d8acd3b7e3c
Bermet0 <bekatktbkv@gmail.com>
2023-12-01 15:11:47 +0600

    problem

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from aiogram.types import BotCommand
 import asyncio
 import logging
-from bot import dp, bot
+from bot import dp, bot, scheduler
 from handlers import (
                       myinfo_router,
                       picture_router,
                       shop_router,
                       start_router,
-                      questions_router
+                      questions_router,
+                      # delayed_answer_router
 )
 from db.queries import init_db, create_tables, populate_tables
 
@@ -24,19 +25,21 @@ async def mine():
         BotCommand(command="myinfo", description="Показать информацию обо мне"),
         BotCommand(command="picture", description="Показать случайную картинку"),
         BotCommand(command="shop", description="Магазин"),
-        BotCommand(command="question", description="Вопрос")
-
+        BotCommand(command="question", description="Вопрос"),
+        BotCommand(command="remind", description="годовщина")
     ])
+
     dp.include_router(start_router)
     dp.include_router(picture_router)
     dp.include_router(myinfo_router)
     dp.include_router(shop_router)
     dp.include_router(questions_router)
+    # dp.include_router(delayed_answer_router)
 
     dp.startup.register(on_startup)
 
+    # scheduler.start()
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
